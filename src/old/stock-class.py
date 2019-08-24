e24dbac6dc870c7b8e7431063c09e7ea9f357d61
Christol Robin <robin@desktop-acln3u6.home>
2019-08-24 18:25:45 +0200

    added datasets / basic examples

--- src/old/stock-class.py
+++ src/old/stock-class.py
@@ -3,20 +3,19 @@ import numpy as np
 
 class Stock():
 
-    def __init__(self, filename, verbose=False, name=None):
+    def __init__(self, filename, verbose=False, name=None, normalize=True):
 
         self.verbose = verbose
         self.filename = filename
-
         self.stocks = dict()
         self.header = None
         self._get_stocks()
-        self._normalize()
+        if normalize:
+            self._normalize()
 
     def _get_stocks(self):
         with open(self.filename, 'r') as f:
             self.header = f.readline().strip('\n').split(',')
-
             for column_type in self.header:
                 self.stocks[column_type] = []
 
@@ -25,6 +24,7 @@ class Stock():
                 line = line.split(',')
                 for column_type, value in zip(self.header, line):
                     self.stocks[column_type].append(value)
+
             for key in self.stocks:
                 try:
                     self.stocks[key] = np.array(
@@ -40,7 +40,6 @@ class Stock():
                 minimum = min(self.stocks[key])
                 frame = max(self.stocks[key]) - minimum
                 self.stocks[key] = (self.stocks[key] - minimum) / frame
-                print(self.stocks[key].sum())
 
     def __getitem__(self, arg):
         return self.stocks[arg]
