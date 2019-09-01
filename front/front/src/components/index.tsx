c70c04805c35063368b4ed79ec5d95043ea1b495
loicttn <loic.titren@hyalin.fr>
2019-09-01 15:05:47 +0200

    Added front init

---
+++ front/front/src/components/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainStocksOverview from './header/mainStocksOverview';
+
+const Index = (): JSX.Element => (
+    <div className="App">
+        <div className="Header">
+            <MainStocksOverview/>
+        </div>
+    </div>
+);
+
+export default Index;
