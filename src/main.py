3347e60576d08c7c7800a2aea54ab9bfc2548ce4
Christol Robin <robin@desktop-acln3u6.home>
2019-09-01 14:29:38 +0200

    added new archi

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+import yfinance as yf
+from tensorflow import keras
+import plotly.graph_objects as go
+
+
+def plot_stocks_pandas(df):
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+                                         open=df['Open'],
+                                         high=df['High'],
+                                         low=df['Low'],
+                                         close=df['Close'])])
+    fig.show()
+
+
+def get_model():
+    model = keras.Sequential()
+    return model
+
+
 if __name__ == "__main__":
-    pass
+    df = yf.download('AAPL')
+    model = get_model()
+    model.fit()
+    print(df.to_numpy())
