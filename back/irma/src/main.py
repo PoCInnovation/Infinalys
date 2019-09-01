b2eb83d59803f78fd805106af00ce352227a5cbf
loicttn <loic.titren@hyalin.fr>
2019-09-01 16:24:18 +0200

    Added new plotting functions

--- back/irma/src/main.py
+++ back/irma/src/main.py
@@ -9,11 +9,11 @@ from tensorflow import keras
 import plotly.graph_objects as go
 
 # Number of time units to predict
-PREDICT  = 30
+PREDICT  = 100
 # AI predicts stocks at t with data from t-LOOKBACK to t-1
 LOOKBACK = 30
 # Number of epochs for training
-EPOCHS   = 1
+EPOCHS   = 20
 
 def plot_stocks_pandas(df):
     fig = go.Figure(data=[go.Candlestick(x=df.index,
@@ -21,6 +21,15 @@ def plot_stocks_pandas(df):
                                          high=df['High'],
                                          low=df['Low'],
                                          close=df['Close'])])
+    fig.show()
+
+def plot_stocks_numpy(nparr):
+    arr = np.swapaxes(nparr, 0,1)
+    fig = go.Figure(data=[go.Candlestick(x=list(range(len(arr))),
+                                         open=arr[0],
+                                         high=arr[1],
+                                         low=arr[2],
+                                         close=arr[3])])
     fig.show()
 
 def normalize_data(dataset):
@@ -32,7 +41,6 @@ def normalize_data(dataset):
         dataset[i] = (dataset[i] - min(dataset[i])) / (max(dataset[i]) - min(dataset[i]))
     dataset = np.swapaxes(dataset, 0, 1)
     return dataset
-
 
 def split_dataset(dataset):
     '''
@@ -53,8 +61,9 @@ def get_model():
     model = keras.Sequential()
     model = keras.models.Sequential()
     model.add(keras.layers.LSTM(128, input_shape=(LOOKBACK, 5), return_sequences=True))
-    model.add(keras.layers.LSTM(128, input_shape=(LOOKBACK, 5), return_sequences=False))
-    model.add(keras.layers.Dropout(0.4))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.LSTM(64, input_shape=(LOOKBACK, 5), return_sequences=True))
+    model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.Dense(5, activation='relu'))
     return model
 
@@ -66,13 +75,13 @@ def compile_model(model):
     model.summary()
     return model
 
-def test_model(model, x, y):
-    res = []
-    for i in range(PREDICT):
-        print(x[:i].shape)
-        res.append(model.predict(x[:i]))
-    for i in range(len(res)):
-        print("Error:", abs(x[i] * 100 / y[i]))
+def test_model(model, x_test, y_test):
+    output = model.predict(np.array([x_test]))
+    error = []
+    for i in range(len(output)):
+        error.append(abs(100 - output[i] * 100 / y_test[i]))
+    error = np.array(error)
+    return np.mean(error)
 
 SAVE = True
 LOAD = False
@@ -87,16 +96,21 @@ if __name__ == "__main__":
     x_train = x_train[:int(len(x_train) - PREDICT)]
     y_test = y_train[int(len(y_train) - PREDICT):]
     y_train = y_train[:int(len(y_train) - PREDICT)]
+    plot_stocks_numpy(x_train)
+    exit()
     model = get_model()
 
     if LOAD:
         model.load_weights(MODEL_PATH)
 
     compile_model(model)
-    print(x_train.shape, x_test.shape, y_test.shape, y_train.shape)
-    exit()
     model.fit(x=x_train, y=y_train, validation_split=0.2, epochs=EPOCHS)
-    test_model(model, x_test, y_test)
-
+    errors_means = []
+    for i in range(PREDICT):
+        errors_means.append(test_model(model, x_test[i], y_test[i]))
+    for mean in errors_means:
+        print('#' + str(errors_means.index(mean)), "\tAverage error", int(mean), "%")
+    print("\nAVERAGE ERROR", np.mean(np.array(errors_means)), "% (calculated on the", PREDICT, "last time units of the dataset)")
+    print("Evaluated loss: ", model.evaluate(x=x_test, y=y_test))
     if SAVE:
         model.save_weights(MODEL_PATH)
