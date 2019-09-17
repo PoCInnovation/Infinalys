2cb1693697aacc7017fd33d07c8153c23bb1dc94
Christol Robin <robin.christol@epitech.eu>
2019-09-17 13:50:24 +0200

    functionnal predictor

--- back/irma/predictor/src/main.py
+++ back/irma/predictor/src/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import os
+import json
+import pandas as pd
 from fetch import download_stocks
 from predict import predict_on_stocks
 
@@ -10,7 +13,35 @@ MODELS_PATH = "./models"
 STOCKS_PATH = "./stocks"
 RESULT_PATH = "/tmp"
 
+from pprint import pprint
+def create_results(stocks_path: str, preds_path: str, results_path: str):
+    dirs = os.listdir(stocks_path)
+    for file in dirs:
+        stock_filepath = os.path.join(stocks_path, file)
+        preds_filepath = os.path.join(preds_path, file)
+        stock = pd.read_csv(stock_filepath, squeeze=True)
+        preds = pd.read_csv(preds_filepath, squeeze=True)
+
+        result = {
+            'stocks': {
+                'Date': list(stock['Date']),
+                'Open': list(stock['Open']),
+                'High': list(stock['High']),
+                'Low': list(stock['Low']),
+                'Close': list(stock['Close']),
+                'Volume': list(stock['Volume']),
+            },
+            'predictions': {
+                'Mean': list(preds['Mean']),
+                'Volume': list(preds['Volume']),
+            }
+        }
+        result_path = os.path.join(results_path, file)
+        with open(result_path, 'w') as fp:
+            json.dump(result, fp)
+
+
 if __name__ == "__main__":
     download_stocks(TEST_COMPANIES_PATH, STOCKS_PATH, max_dl=100)
     predict_on_stocks(STOCKS_PATH, PREDICTIONS_PATH, MODELS_PATH)
-    # create_results(STOCKS_PATH, PREDICTIONS_PATH, RESULT_PATH)
+    create_results(STOCKS_PATH, PREDICTIONS_PATH, RESULT_PATH)
