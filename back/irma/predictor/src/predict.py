7e548ce5fc02329a6de2f7d59db8e5d0d803a98a
Christol Robin <robin@localhost.localdomain>
2019-09-17 13:50:24 +0200

    functionnal predictor

--- back/irma/predictor/src/predict.py
+++ back/irma/predictor/src/predict.py
@@ -4,9 +4,16 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import RepeatVector
+from tensorflow.keras.layers import TimeDistributed
+
 EPOCHS = 1
 LOOKBACK = 20
-PREDICT = 20
+PREDICT = 4
 LOADING = (',-\'', '---', '\'-,', ' | ')
 LEN_LOADING = len(LOADING)
 
@@ -21,17 +28,6 @@ def print_advancement(file: str, curr: int, total: int):
         current_percent + '_' * (20 - current_percent)
     print(f'{advancement}| ', end='')
     print(f'Predicting on file {file} ...', end='', flush=True)
-
-
-class PrintLogs(keras.callbacks.Callback):
-    def __init__(self, epochs):
-        self.epochs = epochs
-
-    def set_params(self, params):
-        params['epochs'] = 0
-
-    def on_epoch_begin(self, epoch, logs=None):
-        print(' Epoch %d/%d' % (epoch + 1, self.epochs), end='', flush=True)
 
 
 def filter_data(raw_data: np.ndarray) -> np.ndarray:
@@ -60,29 +56,30 @@ def normalize_data(data: np.ndarray) -> np.ndarray:
     return data
 
 
-def split_data(data) -> (np.ndarray, np.ndarray):
-    """Split dataset and format it for the lstm model"""
-
-    x, y = [], []
-    for i in range(LOOKBACK, len(data)):
-        x.append(data[i-LOOKBACK:i])
-        y.append(data[i])
-    return np.array(x), np.array(y)
-
-
-def get_model():
+def get_model(n_steps_in: int, n_steps_out: int, n_features: int):
     """Defines LSTM model"""
 
-    model = keras.Sequential()
-    model = keras.models.Sequential([
-        keras.layers.LSTM(128, input_shape=(
-            LOOKBACK, 2), return_sequences=True),
-        keras.layers.Dropout(0.3),
-        keras.layers.LSTM(64, input_shape=(LOOKBACK, 2),
-                          return_sequences=False),
-        keras.layers.Dropout(0.3),
-        keras.layers.Dense(2, activation='relu')])
+    model = Sequential()
+    model.add(LSTM(200, activation='relu', input_shape=(n_steps_in, n_features)))
+    model.add(RepeatVector(n_steps_out))
+    model.add(Dropout(0.3))
+    model.add(LSTM(200, activation='relu', return_sequences=True))
+    model.add(TimeDistributed(Dense(n_features)))
+    model.compile(optimizer='adam', loss='mse')
     return model
+
+
+def split_sequences(sequences, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequences)):
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+        if out_end_ix > len(sequences):
+            break
+        seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix:out_end_ix, :]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
 
 
 def predict_on_stocks(stocks_path: str, store_path: str, models_path: str):
@@ -98,11 +95,15 @@ def predict_on_stocks(stocks_path: str, store_path: str, models_path: str):
     """
 
     import logging
+    # removes any tf warining
     logging.disable(logging.WARNING)
     os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
     dirs = os.listdir(stocks_path)
     nb_predictions = len(dirs)
+
+    n_steps_in, n_steps_out = LOOKBACK, PREDICT
+    n_features = 2
 
     for i, file in enumerate(dirs):
         print_advancement(os.path.join(stocks_path, file), i, nb_predictions)
@@ -110,21 +111,28 @@ def predict_on_stocks(stocks_path: str, store_path: str, models_path: str):
         data = pd.read_csv(os.path.join(stocks_path, file)).to_numpy()
         data = filter_data(data)
         data = normalize_data(data)
+        x_train, y_train = split_sequences(data, n_steps_in, n_steps_out)
 
-        model = get_model()
+        model = get_model(n_steps_in, n_steps_out, n_features)
         model.compile(optimizer='adam', loss='mse')
 
-        x_train, y_train = split_data(data)
         model.fit(x=x_train, y=y_train,
                   epochs=EPOCHS,
-                  verbose=0,
-                  batch_size=32,
-                  callbacks=[PrintLogs(EPOCHS)])
+                  verbose=0)
 
         model_name = os.path.join(
             models_path, os.path.splitext(file)[0] + '.h5')
         model.save(model_name)
 
-        if i == len(dirs):
+        # model = keras.models.load_model(model_name)
+
+        x_input = x_train[-1]
+        x_input = x_input.reshape((1, n_steps_in, n_features))
+        yhat = model.predict(x_input, verbose=0)
+
+        predpath = os.path.join(store_path, file)
+        np.savetxt(predpath, yhat[0], delimiter=',', header='Mean,Volume', comments='')
+
+        if i == len(dirs) - 1:
             print_advancement(os.path.join(
-                stocks_path, file), i, nb_predictions)
+                stocks_path, file), i + 1, nb_predictions)
