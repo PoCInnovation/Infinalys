b25cdeaa16cbbf42dddceea46e0408404565d394
loicttn <loic.titren@hyalin.fr>
2019-09-18 23:24:28 +0200

    Added stock fetching and vizualisation in emma-front

---
+++ front/src/types/StockValues.tsx
@@ -0,0 +1,10 @@
+interface StockValues {
+    Open: Array<number>,
+    Close: Array<number>,
+    High: Array<number>,
+    Low: Array<number>,
+    Volume: Array<number>,
+    Date: Array<number>,
+};
+
+export default StockValues;
